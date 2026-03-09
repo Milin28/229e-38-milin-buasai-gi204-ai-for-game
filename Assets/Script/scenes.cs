84818a7036989692936f4299a930db38654a7147
Milin28 <milin.buas@bumail.net>
2026-03-09 16:59:47 +0700

    v3

---
+++ Assets/Script/scenes.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    public void StartBut()
+    {
+        SceneManager.LoadScene("01");
+    }
+
+    
+}
